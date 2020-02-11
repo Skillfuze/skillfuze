0ff9efaea56aa890aad22144ffd4edac5f24d93c
Khaled Mohamed <khal3d.mohamed@gmail.com>
2020-02-11 17:17:38 +0200

    chore: project initialization (#2)
    * chore: project initialization
    
    - eslint config
    - prettier config
    - ts config
    - lerna setup
    
    Resolves: #1
    
    * feat: wip docusaurus initialization

---
+++ website/src/pages/index.js
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import classnames from 'classnames';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+function Home() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  return (
+    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+      <main className={classnames('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={classnames('button button--outline button--secondary button--lg', styles.getStarted)}
+              to={useBaseUrl('docs')}
+            >
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
