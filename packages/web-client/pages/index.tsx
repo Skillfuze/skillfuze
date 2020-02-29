bc2cd8a93ed5b7c71fda11d2791ec0b2a684681c
Khaled Mohamed <Khal3d.Mohamed@gmail.com>
2020-03-01 00:23:33 +0200

    chore: web-client next.js project init
    Resolves: SF-28

---
+++ packages/web-client/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Home = (): JSX.Element => (
+  <div className="container">
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      <h1>Skillfuze Web Client</h1>
+    </main>
+  </div>
+);
+
+export default Home;
