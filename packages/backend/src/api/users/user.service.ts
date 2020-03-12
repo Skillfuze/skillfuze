dff167eda728e2fdf214fd4cfea1aba208f0fdbe
Khaled Mohamed <khal3d.mohamed@gmail.com>
2020-03-12 14:14:34 +0200

    [SF-32] Register Backend API (#5)
    * chore: project initialization
    
    - eslint config
    - prettier config
    - ts config
    - lerna setup
    
    Resolves: #1
    
    * feat: wip docusaurus initialization
    
    * feat: initialize backend & db integration
    
    Resolves: SF-27
    
    * wip: register
    
    * feat: register backend
    
    Resolves: SF-32
    
    * fix: name in test file
    
    Co-authored-by: Rawan Ashraf <Rawanelashry@gmail.com>

--- packages/backend/src/api/users/user.service.ts
+++ packages/backend/src/api/users/user.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
+import { UserRegisterDTO } from './dtos';
+import { User } from './user.entity';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
+
+  public async register(payload: UserRegisterDTO): Promise<User> {
+    const user = this.userRepository.create(payload);
+    await this.userRepository.save(user);
+
+    return user;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    return this.userRepository.findOne({ email });
+  }
 }
