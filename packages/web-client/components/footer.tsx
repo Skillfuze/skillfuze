c13909353a959a2eb121a18155fe565bde5f9b09
karimelsayed98 <karim.abdelghaffar98@gmail.com>
2020-03-12 22:26:55 +0200

    feat: added: Login frontend
    added: login page with auth services, header & footer components, basic css
    
    Resolves: SF-6, SF-8, SF-10

---
+++ packages/web-client/components/footer.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <div className="flex flex-row">
+      <p className=" ml-8 font-sans text-sm text-white text-left">© Skillfuze 2020</p>
+    </div>
+  );
+};
