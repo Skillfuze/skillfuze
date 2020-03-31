e759212277eeef12ff39fb289c428e7ee304493f
Karim Elsayed <Karim.Abdelghaffar98@gmail.com>
2020-03-31 22:53:23 +0200

    [SF-6, SF-8, SF-10] Login (#9)
    * feat: Login
    Resolves: [SF-6, SF-8, SF-10]
    
    backend: Added { JWT authentication, Auth guards, login endpoint, authService tests and e2e tests for login}
    frontend: Added { login Page with auth services, withAuth HOC, footer component, CSS configs }

--- packages/web-client/components/Layout/Layout.tsx
+++ packages/web-client/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '../Navbar';
+import { Footer } from '../Footer';
 
 interface Props {
   children?: React.ReactNode;
@@ -10,6 +11,9 @@ const Layout: React.FC<Props> = (props: Props) => (
   <div className="flex flex-col min-h-screen">
     <Navbar controls={props.navControls} />
     {props.children}
+    <div className="flex flex-row w-full h-12 bg-primary items-center ">
+      <Footer />
+    </div>
   </div>
 );
 
