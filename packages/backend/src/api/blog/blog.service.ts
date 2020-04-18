80976a825f77bfa93a248781a39ac8fda9401acb
Khaled Mohamed <Khal3d.Mohamed@gmail.com>
2020-04-18 13:19:09 +0200

    feat: blogs events emitter
    resolves: SF-39

--- packages/backend/src/api/blog/blog.service.ts
+++ packages/backend/src/api/blog/blog.service.ts
@@ -2,15 +2,27 @@ import { Injectable, NotFoundException, ForbiddenException } from '@nestjs/commo
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import slugify from 'slugify';
 import * as shortid from 'shortid';
+import axios from 'axios';
 
 import { Blog } from './blog.entity';
 import { BlogRepository } from './blog.repository';
+import { BlogsEventEmitter } from './blogs.eventemitter';
+import config from '../../../config';
 
 @Injectable()
 export class BlogService extends TypeOrmCrudService<Blog> {
   /* istanbul ignore next */
-  constructor(private readonly repository: BlogRepository) {
+  constructor(private readonly repository: BlogRepository, private readonly emitter: BlogsEventEmitter) {
     super(repository);
+
+    this.emitter
+      .on('publish', () => this.buildGatsby())
+      .on('delete', () => this.buildGatsby())
+      .on('update', (blog: Blog) => {
+        if (blog.publishedAt) {
+          this.buildGatsby();
+        }
+      });
   }
 
   public async publish(blogId: number, userId: number): Promise<Blog> {
@@ -41,5 +53,11 @@ export class BlogService extends TypeOrmCrudService<Blog> {
 
   private generateUrl(title: string): string {
     return `${slugify(title)}-${shortid.generate()}`;
+  }
+
+  public async buildGatsby(): Promise<void> {
+    if (config.gatsby.buildHookURL) {
+      axios.post(config.gatsby.buildHookURL);
+    }
   }
 }
