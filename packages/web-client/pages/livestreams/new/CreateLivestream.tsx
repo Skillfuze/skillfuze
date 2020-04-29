6678dfe5372b0bbb54af28b6af01fd3e53329ccc
MariamKamel <mariammkamel98@gmail.com>
2020-04-29 18:08:02 +0200

    feat(frontend): create live stream [ci skip]

---
+++ packages/web-client/pages/livestreams/new/CreateLivestream.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { Button, Input } from '@skillfuze/ui-components';
+import withAuth from '../../../utils/withAuth/withAuth';
+import Layout from '../../../components/Layout';
+import LivestreamService from '../../../services/livestreams.service';
+
+const CreateLiveStream = () => {
+  const livestreamService = LivestreamService.instance;
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [thumbnailURL, setThumbnailURL] = useState('');
+  /* TAGSS TO DOO */
+  const [tags, setTags] = useState('');
+  const [streamKey, setStreamKey] = useState('');
+  const [serverURl, setServerUrl] = useState('skillfulze.com/livestreams');
+  const [error, setError] = useState(undefined);
+
+  const handleSubmit = async (event): Promise<void> => {
+    event.preventDefault();
+    try {
+      const { streamK } = await livestreamService.create({ title, description, thumbnailURL });
+      setStreamKey(streamK);
+    } catch (err) {
+      setError('Cannot create key');
+    }
+  };
+  return (
+    <Layout>
+      <div className="flex flex-grow px-40 items-center justify-center h-full">
+        <div className="flex  w-1/2">
+          <form className="flex flex-col w-2/3" onSubmit={handleSubmit}>
+            <h1 className="text-2xl font-bold text-left">New Livestream</h1>
+            <Input className="mt-8 w-full" value={title} type="text" placeholder="Title" onChange={setTitle} />
+            <Input
+              className="mt-8 w-full"
+              value={description}
+              type="text"
+              placeholder="Description"
+              onChange={setDescription}
+            />
+            <Input
+              className="mt-8 w-full"
+              value={thumbnailURL}
+              type="url"
+              placeholder="ThumbnilURL"
+              onChange={setThumbnailURL}
+            />
+            <Input className="mt-8 w-full" value={tags} type="text" placeholder="tags" onChange={setTags} />
+            <Button className="mt-6 p-2 w-full" type="submit">
+              Generate Key
+            </Button>
+            {error && <p className="text-xs text-warning text-right mt-1">{error}</p>}
+          </form>
+        </div>
+        <div className="flex flex-col items-center w-1/2">
+          <div className="w-2/3 ">
+            <h3 className="font-bold text-xl mb-2">Guide:</h3>
+            <p>Copy and paste the generated key and the server URL into your favorite streaming software</p>
+            <h3 className=" mt-8 mb-2 w-full font-semibold text-lg">Stream Key</h3>
+            <Input value={streamKey} type="text" />
+
+            <h3 className=" mt-6 mb-2 w-full font-semibold text-lg">Server URL</h3>
+            <Input value={serverURl} type="url" />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+export default withAuth({
+  // redirectOnAuthSuccess: '/login',
+})(CreateLiveStream);
---
+++ packages/web-client/services/livestreams.service.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-underscore-dangle */
+import axios from 'axios';
+
+export default class LivestreamService {
+  public user: undefined;
+
+  private static _instance: LivestreamService;
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private constructor() {}
+
+  static get instance(): LivestreamService {
+    if (!this._instance) {
+      this._instance = new LivestreamService();
+    }
+    return this._instance;
+  }
+
+  async create(payload: any): Promise<any> {
+    try {
+      const res = await axios.post('/api/v1/livestreams/', payload);
+      return res.data;
+    } catch (err) {
+      throw err.response;
+    }
+  }
+}
