eec1b1919c69789ae68420552d4975a275f56c54
Rawan A. El Ashry <rawanelashry@gmail.com>
2020-04-30 17:26:36 +0200

    feat(frontend): create livestream

--- packages/web-client/pages/livestreams/new/CreateLivestream.tsx
+++ packages/web-client/pages/livestreams/new/CreateLivestream.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Input } from '@skillfuze/ui-components';
+import { Input, TagsInput, Button } from '@skillfuze/ui-components';
 import withAuth from '../../../utils/withAuth/withAuth';
 import Layout from '../../../components/Layout';
 import LivestreamService from '../../../services/livestreams.service';
@@ -10,8 +10,7 @@ const CreateLiveStream = () => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [thumbnailURL, setThumbnailURL] = useState('');
-  /* TAGSS TO DOO */
-  const [tags, setTags] = useState('');
+  const [tags, setTags] = useState([]);
   const [streamKey, setStreamKey] = useState('');
   const [error, setError] = useState<any>({});
   const serverURL = config.streamingServerURL;
@@ -28,7 +27,7 @@ const CreateLiveStream = () => {
     <Layout>
       <div className="flex flex-grow px-40 items-center justify-center h-full">
         <div className="flex flex-col items-center w-1/2">
-          <form className="flex flex-col w-2/3" onSubmit={handleSubmit}>
+          <div className="flex flex-col w-2/3">
             <h1 className="text-2xl font-bold text-left">New Livestream</h1>
             <Input className="mt-8 w-full" value={title} type="text" placeholder="Title" onChange={setTitle} />
             <Input
@@ -46,18 +45,18 @@ const CreateLiveStream = () => {
               placeholder="ThumbnilURL"
               onChange={setThumbnailURL}
             />
-            <Input className="mt-8 w-full" value={tags} type="text" placeholder="tags" onChange={setTags} />
-            <Button className="mt-6 p-2 w-full" type="submit">
+            <TagsInput tags={tags} onChange={setTags} limit={5} className="mt-8 w-full" />
+            <Button className="mt-6 p-2 w-full" type="submit" onClick={handleSubmit}>
               Generate Key
             </Button>
-          </form>
+          </div>
         </div>
         <div className="flex flex-col items-center w-1/2">
           <div className="w-2/3 ">
             <h3 className="font-bold text-xl mb-2">Guide:</h3>
             <p>Copy and paste the generated key and the server URL into your favorite streaming software</p>
             <h3 className=" mt-8 mb-2 w-full font-semibold text-lg">Stream Key</h3>
-            <Input value={streamKey} type="text" />
+            <Input value={streamKey} disabled type="text" />
 
             <h3 className=" mt-6 mb-2 w-full font-semibold text-lg">Server URL</h3>
             <Input value={serverURL} type="url" />
