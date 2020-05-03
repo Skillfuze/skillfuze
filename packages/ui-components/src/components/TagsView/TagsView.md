943bf3c75851b68fc736410233e2014238da4023
MariamKamel <mariammkamel98@gmail.com>
2020-05-04 01:01:07 +0200

    feat: Livestream view added
    *tags view component added
    *livestream view front-end added
    
    resolves: SF-54

---
+++ packages/ui-components/src/components/TagsView/TagsView.md
@@ -0,0 +1,11 @@
+## Examples
+
+### Basic Usage
+
+```jsx
+const [tags, setTags] = React.useState(['Tag 1', 'Tag 2']);
+
+<div className="container">
+  <TagsView tags={tags}  />
+</div>
+```
---
+++ packages/web-client/pages/livestreams/[livestreamId]/ViewLivestream.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Avatar, Button, TagsView, TagsInput } from '@skillfuze/ui-components';
+import Layout from '../../../components/Layout';
+import LivestreamService from '../../../services/livestreams.service';
+import More from '../../../assets/icons/More.svg';
+
+interface Props {
+  stream: any;
+}
+
+const ViewLivestream = ({ stream }: Props) => {
+  const onClickMore = () => {
+    console.log('More');
+  };
+  return (
+    <Layout>
+      <div className="flex flex-col flex-grow h-screen items-center">
+        {/* video player */}
+        <div className="flex flex-grow text-center h-64 w-5/6 bg-black" />
+
+        <h1 className="flex text-left w-5/6 mt-4">{stream.title}</h1>
+        <p className="text-left  text-warning w-5/6 mt-2 ml-2">Live Now</p>
+        <hr className="flex w-5/6 mt-6 text-grey" />
+
+        <div className=" text-center bg-gray w-5/6 p-1">
+          <div className="flex flex-row h-16 items-center ">
+            <Avatar URL={stream.streamer.avatar} alt="streamer photo" />
+            <div className="ml-4 flex-col flex-auto">
+              <div className="flex flex-row items-center">
+                <div className="flex flex-row flex-grow">
+                  <p className="self-center inline-block font-bold">{`${stream.streamer.firstName} ${stream.streamer.lastName}`}</p>
+                </div>
+                <Button className="m-4" size="small" variant="outlined">
+                  Follow
+                </Button>
+                <More onClick={onClickMore} style={{ cursor: 'pointer' }} />
+              </div>
+            </div>
+          </div>
+
+          <h4 className="flex text-left w-3/5  ml-16 text-grey-dark">{stream.description}</h4>
+          <div className="flex text-left w-3/5 mt-4 ml-16">
+            <TagsView tags={stream.tags} />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+ViewLivestream.getInitialProps = async ctx => {
+  const livestreamService = new LivestreamService();
+  const stream = await livestreamService.getOne(ctx.query.livestreamId.toString());
+  // DTO
+  return { stream };
+};
+export default ViewLivestream;
