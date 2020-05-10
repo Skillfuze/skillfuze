ad9e8bbcd5f24874186e2f46532eabbf6dc51051
Khaled Mohamed <Khal3d.Mohamed@gmail.com>
2020-05-10 03:14:37 +0200

    fix: web client .env loading

--- packages/web-client/next.config.js
+++ packages/web-client/next.config.js
@@ -1,12 +1,7 @@
 /* eslint-disable */
-const nextEnv = require('next-env');
-const dotenvLoad = require('dotenv-load');
+const isProd = process.env.NODE_ENV === 'production';
 
-dotenvLoad();
-
-const withNextEnv = nextEnv();
-
-module.exports = withNextEnv({
+module.exports = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -14,5 +9,12 @@ module.exports = withNextEnv({
     });
 
     return config;
-  }
-});
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_BLOGS_CLIENT: process.env.NEXT_PUBLIC_BLOGS_CLIENT,
+    NEXT_PUBLIC_RTMP_SERVER_URL: process.env.NEXT_PUBLIC_RTMP_SERVER_URL,
+    NEXT_PUBLIC_STREAMING_SERVER_URL: process.env.NEXT_PUBLIC_STREAMING_SERVER_URL
+  },
+  assetPrefix: isProd ? 'https://static.skillfuze.com' : ''
+};
