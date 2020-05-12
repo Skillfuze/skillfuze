a7cec48893d923b370e1c309fd26ee4b45cdb313
Khaled Mohamed <khal3d.mohamed@gmail.com>
2020-05-12 23:42:18 +0200

    feat(streaming): implement live streaming server (#25)
    resolves: [SF-52]
    
    
    Co-authored-by: Karim Elsayed <Karim.Abdelghaffar98@gmail.com>
    Co-authored-by: Chantal Chawki <chanty98@hotmail.com>

--- packages/blogs-client/config/index.js
+++ packages/blogs-client/config/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-   API_URL: process.env.API_URL || 'http://localhost:3000',
+   API_URL: process.env.GATSBY_API_URL || 'http://localhost:3000',
 };
--- packages/web-client/config/index.ts
+++ packages/web-client/config/index.ts
@@ -1,5 +1,6 @@
 const config = {
   blogsClientUrl: process.env.BLOGS_CLIENT_URL || 'http://localhost:3002',
-  streamingServerURL: process.env.STREAMING_SERVER_URL || 'http://localhost:3002',
+  streamingServerURL: process.env.RTMP_SERVER_URL || 'rtmp://localhost:1935/live',
+  httpStreamingServerURL: process.env.STREAMING_SERVER_URL || 'http://localhost:8080/live',
 };
 export default config;
