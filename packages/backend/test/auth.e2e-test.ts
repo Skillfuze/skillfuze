71aaa55b693529848c51f0f425d4773841a5ad00
Khaled Mohamed <Khal3d.Mohamed@gmail.com>
2020-05-12 23:58:35 +0200

    fix: supertest import

--- packages/backend/test/auth.e2e-test.ts
+++ packages/backend/test/auth.e2e-test.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ValidationPipe, INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthController } from '../src/api/auth/auth.controller';
