c7152d0e56776392b89d10d29d88309bb7880102
Karim Elsayed <Karim.Abdelghaffar98@gmail.com>
2020-06-03 14:23:26 +0200

    Home page components [SF-68] (#34)
    feat(ui-kit): Home page Components [SF-68]

--- packages/ui-components/postcss.config.js
+++ packages/ui-components/postcss.config.js
@@ -4,7 +4,7 @@ module.exports = {
     require('tailwindcss')('./tailwind.config.js'),
     require('autoprefixer'),
     require('@fullhuman/postcss-purgecss')({
-      content: ['./src/components/**/*.tsx', './src/components/**/*.md']
+      content: ['./src/components/**/*.tsx', './src/components/**/*.md', 'node_modules/react-multi-carousel/**/*.js']
     })
   ]
 }
---
+++ packages/ui-components/src/components/ContentCard/styles.ts
@@ -0,0 +1,22 @@
+import { css } from 'emotion';
+
+export const headerContainerStyle = css`
+  position: relative;
+  text-align: center;
+`;
+export const isLiveStyle = css`
+  position: absolute;
+  top: 8px;
+  left: 16px;
+  background: var(--color-warning);
+  color: white;
+  border-radius: 0.2rem;
+`;
+export const viewsStyle = css`
+  position: absolute;
+  top: 8px;
+  right: 10px;
+  background: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 0.2rem;
+`;
