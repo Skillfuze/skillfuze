12090c30d838586be70d2d0f491b9e901ab68a18
Chantal Chawki <chantal.chawki@gmail.com>
2020-08-11 23:34:04 +0200

    feat: add analytics service [WIP]

--- packages/backend/config/winston.config.ts
+++ packages/backend/config/winston.config.ts
@@ -1,5 +1,6 @@
 import * as winston from 'winston';
 import 'winston-daily-rotate-file';
+import os from 'os';
 
 export const winstonOptions = {
   console: {
@@ -15,6 +16,21 @@ export const winstonOptions = {
     maxSize: '20m',
     maxFiles: '14d',
     createSymlink: true,
+    format: winston.format.combine(
+      winston.format.uncolorize(),
+      winston.format.timestamp(),
+      winston.format.metadata({ fillExcept: ['timestamp', 'level', 'message'] }),
+      winston.format.json(),
+    ),
+  },
+  logdna: {
+    key: '',
+    hostname: 'skillfuze.com',
+    ip: os.networkInterfaces().lo0[0].address,
+    mac: os.networkInterfaces().lo0[0].mac,
+    app: 'skillfuze',
+    level: 'info',
+    index_meta: true,
     format: winston.format.combine(
       winston.format.uncolorize(),
       winston.format.timestamp(),
--- packages/backend/src/utils/logger.ts
+++ packages/backend/src/utils/logger.ts
@@ -1,10 +1,12 @@
 import * as winston from 'winston';
+import logdnaWinston from 'logdna-winston';
 import config from '../../config';
 
 export const logger = winston.createLogger({
   transports: [
     new winston.transports.DailyRotateFile(config.winston.dailyRotateFile),
     new winston.transports.Console(config.winston.console),
+    new logdnaWinston(config.winston.logdna),
   ],
   exitOnError: false,
 });
