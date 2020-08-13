e59c6520ce501d464d5a26351e6c343c60a6d72c
Karim Elsayed <Karim.Abdelghaffar98@gmail.com>
2020-08-13 18:27:40 +0200

    resolve comment

--- packages/types/src/dtos/update-profile.dto.ts
+++ packages/types/src/dtos/update-profile.dto.ts
@@ -1,10 +1,10 @@
 export interface UpdateProfileDTO {
-  firstName: string;
-  lastName: string;
-  username: string;
-  email: string;
-  bio: string;
-  password: string;
-  confirmPassword: string;
-  avatarURL: string;
+  firstName?: string;
+  lastName?: string;
+  username?: string;
+  email?: string;
+  bio?: string;
+  password?: string;
+  confirmPassword?: string;
+  avatarURL?: string;
 }
